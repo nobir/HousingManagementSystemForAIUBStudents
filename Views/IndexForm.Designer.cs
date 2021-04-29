69ee1669e53734f4c7c0ba6870aba7b77a791222
Nobir <nobirfreelancer@gmail.com>
2021-04-29 23:52:21 +0600

    Design added for Login Form

--- Views/IndexForm.Designer.cs
+++ Views/IndexForm.Designer.cs
@@ -31,8 +31,22 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IndexForm));
             this.tabControlIndex = new System.Windows.Forms.TabControl();
             this.tabLogin = new System.Windows.Forms.TabPage();
+            this.lbEmail = new System.Windows.Forms.Label();
             this.tabRegistration = new System.Windows.Forms.TabPage();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.gbLoginType = new System.Windows.Forms.GroupBox();
+            this.rbTenant = new System.Windows.Forms.RadioButton();
+            this.rbRenter = new System.Windows.Forms.RadioButton();
+            this.rbAdmin = new System.Windows.Forms.RadioButton();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.errMsgEmail = new System.Windows.Forms.Label();
+            this.errMsgPassword = new System.Windows.Forms.Label();
+            this.lbUserType = new System.Windows.Forms.Label();
             this.tabControlIndex.SuspendLayout();
+            this.tabLogin.SuspendLayout();
+            this.gbLoginType.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControlIndex
@@ -50,6 +64,15 @@
             // 
             // tabLogin
             // 
+            this.tabLogin.Controls.Add(this.lbUserType);
+            this.tabLogin.Controls.Add(this.errMsgPassword);
+            this.tabLogin.Controls.Add(this.errMsgEmail);
+            this.tabLogin.Controls.Add(this.btnLogin);
+            this.tabLogin.Controls.Add(this.gbLoginType);
+            this.tabLogin.Controls.Add(this.tbPassword);
+            this.tabLogin.Controls.Add(this.lbPassword);
+            this.tabLogin.Controls.Add(this.tbEmail);
+            this.tabLogin.Controls.Add(this.lbEmail);
             this.tabLogin.Location = new System.Drawing.Point(4, 30);
             this.tabLogin.Name = "tabLogin";
             this.tabLogin.Padding = new System.Windows.Forms.Padding(3);
@@ -57,6 +80,15 @@
             this.tabLogin.TabIndex = 0;
             this.tabLogin.Text = "Login";
             this.tabLogin.UseVisualStyleBackColor = true;
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(65, 69);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(48, 21);
+            this.lbEmail.TabIndex = 0;
+            this.lbEmail.Text = "Email";
             // 
             // tabRegistration
             // 
@@ -68,6 +100,115 @@
             this.tabRegistration.Text = "Registration";
             this.tabRegistration.UseVisualStyleBackColor = true;
             // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(196, 62);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(293, 28);
+            this.tbEmail.TabIndex = 1;
+            this.tbEmail.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbEmail_KeyUp);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(196, 142);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(293, 28);
+            this.tbPassword.TabIndex = 2;
+            this.tbPassword.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbPassword_KeyUp);
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(65, 149);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(77, 21);
+            this.lbPassword.TabIndex = 2;
+            this.lbPassword.Text = "Password";
+            // 
+            // gbLoginType
+            // 
+            this.gbLoginType.Controls.Add(this.rbAdmin);
+            this.gbLoginType.Controls.Add(this.rbRenter);
+            this.gbLoginType.Controls.Add(this.rbTenant);
+            this.gbLoginType.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gbLoginType.Location = new System.Drawing.Point(196, 217);
+            this.gbLoginType.Name = "gbLoginType";
+            this.gbLoginType.Size = new System.Drawing.Size(445, 58);
+            this.gbLoginType.TabIndex = 3;
+            this.gbLoginType.TabStop = false;
+            // 
+            // rbTenant
+            // 
+            this.rbTenant.AutoSize = true;
+            this.rbTenant.Checked = true;
+            this.rbTenant.Location = new System.Drawing.Point(7, 20);
+            this.rbTenant.Name = "rbTenant";
+            this.rbTenant.Size = new System.Drawing.Size(86, 25);
+            this.rbTenant.TabIndex = 3;
+            this.rbTenant.TabStop = true;
+            this.rbTenant.Text = "Student";
+            this.rbTenant.UseVisualStyleBackColor = true;
+            // 
+            // rbRenter
+            // 
+            this.rbRenter.AutoSize = true;
+            this.rbRenter.Location = new System.Drawing.Point(179, 20);
+            this.rbRenter.Name = "rbRenter";
+            this.rbRenter.Size = new System.Drawing.Size(77, 25);
+            this.rbRenter.TabIndex = 4;
+            this.rbRenter.Text = "Renter";
+            this.rbRenter.UseVisualStyleBackColor = true;
+            // 
+            // rbAdmin
+            // 
+            this.rbAdmin.AutoSize = true;
+            this.rbAdmin.Location = new System.Drawing.Point(353, 20);
+            this.rbAdmin.Name = "rbAdmin";
+            this.rbAdmin.Size = new System.Drawing.Size(77, 25);
+            this.rbAdmin.TabIndex = 5;
+            this.rbAdmin.Text = "Admin";
+            this.rbAdmin.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Enabled = false;
+            this.btnLogin.Location = new System.Drawing.Point(196, 328);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(150, 35);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btnLogin_KeyUp);
+            // 
+            // errMsgEmail
+            // 
+            this.errMsgEmail.AutoSize = true;
+            this.errMsgEmail.Font = new System.Drawing.Font("Calibri", 9.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errMsgEmail.ForeColor = System.Drawing.Color.Red;
+            this.errMsgEmail.Location = new System.Drawing.Point(192, 93);
+            this.errMsgEmail.Name = "errMsgEmail";
+            this.errMsgEmail.Size = new System.Drawing.Size(0, 19);
+            this.errMsgEmail.TabIndex = 7;
+            // 
+            // errMsgPassword
+            // 
+            this.errMsgPassword.AutoSize = true;
+            this.errMsgPassword.Font = new System.Drawing.Font("Calibri", 9.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errMsgPassword.ForeColor = System.Drawing.Color.Red;
+            this.errMsgPassword.Location = new System.Drawing.Point(192, 173);
+            this.errMsgPassword.Name = "errMsgPassword";
+            this.errMsgPassword.Size = new System.Drawing.Size(0, 19);
+            this.errMsgPassword.TabIndex = 8;
+            // 
+            // lbUserType
+            // 
+            this.lbUserType.AutoSize = true;
+            this.lbUserType.Location = new System.Drawing.Point(65, 241);
+            this.lbUserType.Name = "lbUserType";
+            this.lbUserType.Size = new System.Drawing.Size(78, 21);
+            this.lbUserType.TabIndex = 9;
+            this.lbUserType.Text = "User Type";
+            // 
             // IndexForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
@@ -75,6 +216,7 @@
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(782, 553);
             this.Controls.Add(this.tabControlIndex);
+            this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
@@ -86,6 +228,10 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "House Management System For AIUB Students";
             this.tabControlIndex.ResumeLayout(false);
+            this.tabLogin.ResumeLayout(false);
+            this.tabLogin.PerformLayout();
+            this.gbLoginType.ResumeLayout(false);
+            this.gbLoginType.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -95,5 +241,17 @@
         private System.Windows.Forms.TabControl tabControlIndex;
         private System.Windows.Forms.TabPage tabLogin;
         private System.Windows.Forms.TabPage tabRegistration;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.GroupBox gbLoginType;
+        private System.Windows.Forms.RadioButton rbAdmin;
+        private System.Windows.Forms.RadioButton rbRenter;
+        private System.Windows.Forms.RadioButton rbTenant;
+        private System.Windows.Forms.Label errMsgEmail;
+        private System.Windows.Forms.Label errMsgPassword;
+        private System.Windows.Forms.Label lbUserType;
     }
 }
--- Views/IndexForm.cs
+++ Views/IndexForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace HousingManagementSystemForAIUBStudents.Views
 {
@@ -15,6 +16,100 @@ namespace HousingManagementSystemForAIUBStudents.Views
         public IndexForm()
         {
             InitializeComponent();
+        }
+
+        protected void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (e.CloseReason != CloseReason.WindowsShutDown)
+            {
+                Application.Exit();
+            }
+        }
+
+
+        /**
+         * Check the Email if it's valid or not
+         * 
+         * @return boolean
+         */
+
+        private bool _IsEmailValid()
+        {
+            return false;
+        }
+
+
+        /**
+         * Check the Email
+         * and show the Error message
+         * in the Form page
+         * if any Error message have
+         * 
+         * @return void
+         */
+
+        private void _CheckEmailValidation()
+        {
+
+        }
+
+
+        /**
+         * Check the Passwrod if it's valid or not
+         * 
+         * @return boolean
+         */
+
+        private bool _IsPasswordValid()
+        {
+            return false;
+        }
+
+
+        /**
+         * Set the Enable properties of Login Button
+         * depending on the validation
+         * if the all the inputs are
+         * valid then Enable properties set to True
+         * otherwise False
+         * 
+         * @return void
+         */
+
+        private void _SetLoginButtonEnableProperties()
+        {
+
+        }
+
+
+        /**
+         * Check the Passwrod
+         * and show the Error message
+         * in the Form page
+         * if any Error message have
+         * 
+         * @return void
+         */
+
+        private void _CheckPasswordValidation()
+        {
+
+        }
+
+        private void tbEmail_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void tbPassword_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void btnLogin_KeyUp(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
